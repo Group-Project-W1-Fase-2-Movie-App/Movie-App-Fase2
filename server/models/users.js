db78468e342611bb33c8caaaa687f166c60bca29
shiranai <regakun836@gmail.com>
2021-03-04 21:03:50 +0900

    unique constraint to email added

--- server/models/users.js
+++ server/models/users.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const { hash } = require('../helpers/password-handler.js')
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     /**
@@ -15,9 +16,41 @@ module.exports = (sequelize, DataTypes) => {
   };
   Users.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.TEXT
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "email is required"
+        },
+        isEmail: {
+          args: true,
+          msg: "email format isnt valid"
+        }
+      }
+    },
+    password:{
+      type: DataTypes.TEXT,
+      allowNull:false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Password is required"
+        },
+        min: {
+          args: 4,
+          msg: "Password has to be at least 4 characters"
+        }
+      }
+    }
   }, {
+    hooks : {
+      beforeCreate(data, opt) {
+        data.password = hash(data.passwordd)
+      }
+    },
     sequelize,
     modelName: 'Users',
   });
