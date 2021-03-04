23f4d6a2fcc86e5a38a0ac1aa988984b4715b35a
shiranai <regakun836@gmail.com>
2021-03-04 21:19:53 +0900

    server-login done

--- server/app.js
+++ server/app.js
@@ -4,12 +4,14 @@ const cors = require("cors")
 const express = require('express')
 const port = process.env.port
 const app = express()
+const router = require('./routes/index.js')
+
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
+app.use('/',router)
 // app.use(errHandler)
 
 app.listen(port, () => {
