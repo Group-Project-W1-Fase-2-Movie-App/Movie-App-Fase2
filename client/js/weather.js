1b21911b4f820894329f047727a1473b189b5eb3
syaisyah <terraaisyah@gmail.com>
2021-03-05 12:16:58 +0700

    weather JQuery

--- client/js/weather.js
+++ client/js/weather.js
@@ -11,20 +11,20 @@ function fetchDataWeather() {
       city: localStorage.city,
     },
   })
-  .done(response => {
-    console.log(response);
-    const lastUpdate = response.current.last_updated
-    const temperature = response.current.temp_c;
-    const text = response.current.condition.text;
-    const imageURL = response.current.condition.icon;
-    $("#lastUpdate").val("lastUpdate")
-    $("#tempetature").val("temperature")
-    $("#text").val("text")
-    $("#iconWeather").attr(src, "imageURL")
-    
-  })
-  .fail(err => {
-    console.log(err)
-  })
+    .done(response => {
+      console.log(response);
+      const lastUpdate = response.current.last_updated
+      const temperature = response.current.temp_c;
+      const textWeather = response.current.condition.text;
+      const imageURL = response.current.condition.icon;
+      $("#lastUpdate").text("lastUpdate")
+      $("#tempetature").text("temperature")
+      $("#text").text("textWeather")
+      $("#iconWeather").attr(src, "imageURL")
+
+    })
+    .fail(err => {
+      console.log(err)
+    })
 }
 
