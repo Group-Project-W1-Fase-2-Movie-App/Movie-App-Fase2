65731a26a7eb85350bbd7cf304c7e91c06be7eac
syaisyah <terraaisyah@gmail.com>
2021-03-05 12:08:56 +0700

    weather client jQuery

--- client/js/weather.js
+++ client/js/weather.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
 })
 
 function fetchDataWeather() {
+
   $.ajax({
     url: 'http://localhost:3000/weather',
     method: 'GET',
@@ -12,16 +13,18 @@ function fetchDataWeather() {
   })
   .done(response => {
     console.log(response);
-    const currentWeather = response.current.condition;
+    const lastUpdate = response.current.last_updated
+    const temperature = response.current.temp_c;
+    const text = response.current.condition.text;
+    const imageURL = response.current.condition.icon;
+    $("#lastUpdate").val("lastUpdate")
+    $("#tempetature").val("temperature")
+    $("#text").val("text")
+    $("#iconWeather").attr(src, "imageURL")
     
-    
-    
-
-
   })
   .fail(err => {
     console.log(err)
   })
-
 }
 
